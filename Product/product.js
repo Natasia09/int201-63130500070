7e5b4d1501f725fa22afb4888372e853052c7df7
Natasia09 <68784670+Natasia09@users.noreply.github.com>
2021-11-01 22:44:39 +0700

    Add files via upload
    headphone

---
+++ Product/product.js
@@ -0,0 +1,49 @@
+const product1 = {
+    id : 'P01',
+    name : 'AirPods Pro',
+    description: 'Active Noise Cancellation for immersive sound.',
+    price : 8990,
+    img:'img/airpod.jpg',
+    stock : 10
+}
+
+const product2 = {
+    id : 'P02',
+    name : 'Huawei FreeBuds Pro True Wireless',
+    description : 'ตัดเสียงรบกวนได้เบาลงถึง 70%',
+    price : 4950,
+    img:'img/huawei.jfif',
+    stock : 9
+}
+
+const product3 = {
+    id : 'P03',
+    name : 'Bose QuietComfort Earbuds True Wireless',
+    description:'แบตเตอรี่อายุการใช้งานยาวนาน 6 ชั่วโมง',
+    price : 10690,
+    img:'img/bosequietcomfort.jpeg',
+    stock : 5
+}
+
+const product4 = {
+    id : 'P04',
+    name : 'marshall',
+    description:'แบตเตอรีใช้งานต่อเนื่อง 5 ชั่วโมง',
+    price : 5990,
+    img:'img/bosequietcomfort.jpeg',
+    stock : 9
+}
+
+const product5 = {
+    id : 'P05',
+    name : 'Xiaomi Redmi AirDots 2',
+    description : 'ส่งสัญญาณไกลไม่มีสะดุด ใช้งานได้ 8-12 ชั่วโมง',
+    price : 3923,
+    img:'img/xiaomi.jpg',
+    stock : 7
+}
+
+export {product1,product2,product3,product4,product5} ;
+
+
+
